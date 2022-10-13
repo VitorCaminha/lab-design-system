7a82f89c2ab0cb84eee6b367d7dc642a94c4480f
Vitor Leao Caminha <vitorcaminha@or.com.br>
2022-10-13 20:09:49 -0300

    Initial commit

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,52 @@
+import { InputHTMLAttributes, ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+
+export  interface TextInputRootProps {
+  children: ReactNode;
+}
+
+function TextInputRoot(props: TextInputRootProps) {
+  return (
+    <div
+      className={clsx(
+        'flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300',
+      )}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+  return (
+    <Slot className='w-6 h-6 text-gray-400'>
+      {props.children}
+    </Slot>
+  )
+}
+
+export interface TextInputFieldProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+function TextInputField(props: TextInputFieldProps) {
+  return (
+    <input
+      className='bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none'
+      {...props}
+    />
+  )
+}
+
+TextInputRoot.displayName = 'TextInput.Root';
+TextInputField.displayName = 'TextInput.Input';
+TextInputIcon.displayName = 'TextInput.Icon';
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputField,
+  Icon: TextInputIcon,
+}
